98a49b28132d7fe9cfb0d465a1b695c90dcd4c2a
Vilem Bednarik <xbednar@nymfe126.fi.muni.cz>
2026-05-07 12:44:57 +0200

    fix: minor adjustments

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -37,7 +37,7 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.safe_load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
     
 def authenticate(password):
@@ -51,17 +51,17 @@ if __name__ == '__main__':
     print("2. Code injection vulnerability:")
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
-    choice  = input("Select vulnerability: ")
+    choice  = raw_input("Select vulnerability: ")
     if choice == "1": 
         new_person = Person("Vickie")  
-        print_nametag(input("Please format your nametag: "), new_person)
+        print_nametag(raw_input("Please format your nametag: "), new_person)
     elif choice == "2":
-        urlib_version = input("Choose version of urllib: ")
-        fetch_website(urlib_version, url="https://www.google.com")
+        urlib_version = raw_input("Choose version of urllib: ")
+        fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
-        load_yaml(input("File name: "))
+        load_yaml(raw_input("File name: "))
         print("Executed -ls on current folder")
     elif choice == "4":
-        password = input("Enter master password: ")
+        password = raw_input("Enter master password: ")
         authenticate(password)
 
